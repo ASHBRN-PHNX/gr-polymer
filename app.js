f066a7e0b647ece807b76bab3acbd1122e2998b6
Alisdair Frost <alisdair.frost@gmail.com>
2019-04-27 22:54:33 -0600

    Add server console logs

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const prplConfig = require('./build/polymer.json');
 const prplServer = restify.createServer();
 
 prplServer.get('/*', prpl.makeHandler('./build/', prplConfig));
-prplServer.listen(80);
+prplServer.listen(80, () => {
+  console.log(`\n\n------------------------------`);
+  console.log(`\t PRPL Server `);
+  console.log(`------------------------------`);
+  console.log(`Listening on port: 80`);
+  console.log(`Server name: prplServer`);
+});
 
 /**
  * Backend Server
@@ -32,5 +38,12 @@ restifyServer.use(restify.plugins.bodyParser());
 restifyServer.use(restify.plugins.queryParser());
 
 restifyServer.listen(config.server.port, () => {
+  console.log(`\n------------------------------`);
+  console.log(`\t RestifyServer `);
+  console.log(`------------------------------`);
+  console.log(`Listening on port: ${config.server.port}`);
+  console.log(`Server name: ${config.server.name}`);
+  console.log(`Environment: ${process.env.NODE_ENV}\n`);
+
   require('./routes/routes')(restifyServer);
 });
