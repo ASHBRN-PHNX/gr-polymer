091e691e2d396a7ba1d9071e1f403af1d7159bad
Alisdair Frost <alisdair.frost@gmail.com>
2019-06-23 15:04:26 -0600

    Finish create User route

--- modules/example/routes/example.v1.routes.js
+++ modules/example/routes/example.v1.routes.js
@@ -1,9 +1,9 @@
-const config = require(`../../../config/config.${process.env.NODE_ENV}.json`);
+const config = require(`../../../config/config.${process.env.NODE_ENV}.js`);
 
 const exampleController = require('../controllers/example.v1.controller.js');
 
 module.exports = app => {
-  const routePath = `${config.BASE_PATH}example`;
+  const routePath = `${config.PATH}example`;
 
   app.get({ path: routePath, version: '1.0.0' }, exampleController.get);
 };
